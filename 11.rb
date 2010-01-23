32c82757f06f0d32f574b627db8e489cb96b2343
fahadsadah <fahadsadah@gmail.com>
2010-01-23 17:11:39 +0000

    Added iterator, x/y distinction

--- 11.rb
+++ 11.rb
@@ -2,12 +2,18 @@
 file = File.new("11.dat", "r")
 grid = Array.new
 (0..19).each do
-	|x|
-	grid[x] = Array.new
+	|y|
+	grid[y] = Array.new
 	i = 0
 	file.gets.split.each do
-		|y|
-		grid[x][i] = y
+		|x|
+		grid[y][i] = x
 		i += 1
+	end
+end
+grid.each do
+	|x|
+	grid[x].each do
+		|y|
 	end
 end
