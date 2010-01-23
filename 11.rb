14b232d9221a03925b6ac595e0da81e8b89a85f6
fahadsadah <fahadsadah@gmail.com>
2010-01-23 17:51:26 +0000

    Completed

--- 11.rb
+++ 11.rb
@@ -7,13 +7,55 @@ grid = Array.new
 	i = 0
 	file.gets.split.each do
 		|x|
-		grid[y][i] = x
+		grid[y][i] = x.to_i
 		i += 1
 	end
 end
-grid.each do
+biggestprod = 0
+(0..19).each do
 	|y|
-	grid[y].each do
+	(0..19).each do
 		|x|
+		#up
+		if y - 3 >= 0 then
+			prod = grid[y][x] * grid[y-1][x] * grid[y-2][x] * grid[y-3][x]
+			biggestprod = prod if prod > biggestprod
+		end
+		#down
+		if y + 3 <= 19 then
+			prod = grid[y][x] * grid[y+1][x] * grid[y+2][x] * grid[y+3][x]
+			biggestprod = prod if prod > biggestprod
+		end
+		#left
+		if x - 3 >= 0 then
+			prod = grid[y][x] * grid[y][x-1] * grid[y][x-2] * grid[y][x-3]
+			biggestprod = prod if prod > biggestprod
+		end
+		#right
+		if x + 3 <= 19 then
+			prod = grid[y][x] * grid[y][x+1] * grid[y][x+2] * grid[y][x+3]
+			biggestprod = prod if prod > biggestprod
+		end
+		#up-left
+		if y - 3 >= 0 && x - 3 >= 0 then
+			prod = grid[y][x] * grid[y-1][x-1] * grid[y-2][x-2] * grid[y-3][x-3]
+			biggestprod = prod if prod > biggestprod
+		end
+		#up-right
+		if y - 3 >= 0 && x + 3 <= 19 then
+			prod = grid[y][x] * grid[y-1][x+1] * grid[y-2][x+2] * grid[y-3][    x+3]
+			biggestprod = prod if prod > biggestprod
+		end
+		#down-left
+		if y + 3 <= 19 && x - 3 >= 0 then
+			prod = grid[y][x] * grid[y+1][x-1] * grid[y+2][x-2] * grid[y+3][    x-3]
+			biggestprod = prod if prod > biggestprod
+		end
+		#down-right
+		if y + 3 <= 19 && x + 3 <= 19 then
+			prod = grid[y][x] * grid[y+1][x+1] * grid[y+2][x+2] * grid[y+3][        x+3]
+			biggestprod = prod if prod > biggestprod
+		end
 	end
 end
+print "biggestprod = " + biggestprod.to_s + "\n"
