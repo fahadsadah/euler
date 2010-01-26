e79b00e7c4a783e5e29303fe489a043676ccdc9d
fahadsadah <fahadsadah@gmail.com>
2010-01-26 17:58:27 +0000

    s/divisors/factors/

--- 12.rb
+++ 12.rb
@@ -1,17 +1,17 @@
 #!/usr/bin/ruby
 n = 1
-divisors = 0
-while divisors < 500 do
+factors = 0
+while factors < 500 do
 	n = n + 1
 	triangle = (n*(n+1))/2
-	divisors = 0
+	factors = 0
 	if Math.sqrt(triangle) % 2 == 0 then
 		to = Math.sqrt(triangle)
 	else
 		to = Math.sqrt(triangle).round
 	end
-	(1..to).each { |x| divisors = divisors + 1 if triangle % x == 0	}
-	divisors = divisors * 2
-	print "#{triangle} has #{divisors} divisors\n"
+	(1..to).each { |x| factors = factors + 1 if triangle % x == 0	}
+	factors = factors * 2
+	print "#{triangle} has #{factors} factors\n"
 end
-print "divisors is #{divisors}\n"
+print "factors is #{factors}\n"
