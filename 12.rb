849b5b3c77779e1be0839953a8cee01e91b78be8
fahadsadah <fahadsadah@gmail.com>
2010-01-26 17:53:26 +0000

    Optimise (2*factors of sqrt(x), rather than just testing up to x/2 and adding 1

--- 12.rb
+++ 12.rb
@@ -5,25 +5,13 @@ while divisors < 500 do
 	n = n + 1
 	triangle = (n*(n+1))/2
 	divisors = 0
-	if triangle % 2 == 0 then
-		to = triangle / 2
+	if Math.sqrt(triangle) % 2 == 0 then
+		to = Math.sqrt(triangle)
 	else
-		to = (triangle / 2).round
+		to = Math.sqrt(triangle).round
 	end
-	print "Factors of #{triangle}: "
-	first = 1
-	(1..to).each do
-		|x|
-		if triangle % x == 0 then
-			divisors = divisors + 1
-			if first == 1 then
-				print x.to_s
-				first = 0
-			else
-				print ", #{x}"
-			end
-		end
-	end
-	print "\n"
+	(1..to).each { |x| divisors = divisors + 1 if triangle % x == 0	}
+	divisors = divisors * 2
+	print "#{triangle} has #{divisors} divisors\n"
 end
 print "divisors is #{divisors}\n"
